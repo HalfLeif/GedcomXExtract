e1d89bfe1218ad72811932870a7464b3524497bd
Leif Schelin <scleif@student.chalmers.se>
2017-03-10 14:58:58 +0100

    Now aggregates records in the same collection because records from the same image could appear in different gz files.

--- src/main/java/se/chalmers/gedcomx/Main.java
+++ src/main/java/se/chalmers/gedcomx/Main.java
@@ -14,39 +14,21 @@ public class Main {
 
     /**
      * Should pass exactly two arguments to main:
-     * 1. Directory of the records containing collections like 1647578, "sweden/records/".
+     * 1. Directory of the records containing collections like 1647578, e.g. "sweden/records/".
      * 2. Name of output directory. Will create a subdirectory for each collection.
+     *
+     * Returns on error.
      * */
     public static void main(String[] args){
         File recordsDir = new File(args[0]);
         File outDir = new File(args[1]);
+        outDir.mkdirs();
 
         for (File collection : recordsDir.listFiles()) {
             if (collection.isDirectory()) {
-                parseGedcomDir(collection, outDir);
-            }
-        }
-    }
-
-    private static void parseGedcomDir(File collectionDir, File outputDir) {
-        String collectionName = collectionDir.getName();
-        System.out.println("Collection "+ collectionName);
-        File outputCollectionDir = new File(outputDir, collectionName);
-        outputCollectionDir.mkdirs();
-
-        File recordsDir = new File(collectionDir, "RecordChunk");
-        for (File gzFile : recordsDir.listFiles()) {
-            if ("gz".equals(FilenameUtils.getExtension(gzFile.getName()))){
-                String newName = FilenameUtils.getBaseName(gzFile.getName());
-                File outfile = new File(outputCollectionDir, newName + ".csv");
                 try {
-                    outfile.createNewFile();
-                    PrintWriter writer = new PrintWriter(outfile);
-                    parseGedcomFile(gzFile, writer);
-                    writer.close();
-
-                    System.out.println("\tWrote " + gzFile.getName() + " to " + outfile.getName());
-                } catch (XMLStreamException | IOException e) {
+                    parseCollection(collection, outDir);
+                } catch (IOException | XMLStreamException e) {
                     e.printStackTrace();
                     return;
                 }
@@ -55,17 +37,47 @@ public class Main {
     }
 
     /**
-     * Parses this gedcomx .gz file into this Appendable.
+     * Aggregates all relevant labels of this collection into a single file
+     * which is created in `outputDir`.
      * */
-    private static void parseGedcomFile(File gzFile, Appendable output) throws IOException, XMLStreamException {
-        RecordAccumulator accumulator = new RecordAccumulator();
+    private static void parseCollection(File collectionDir, File outputDir)
+            throws IOException, XMLStreamException {
+        String collectionName = collectionDir.getName();
+        System.out.println("Collection "+ collectionName);
+
+        RecordAccumulator collectionLabels = new RecordAccumulator();
+        File recordsDir = new File(collectionDir, "RecordChunk");
+        for (File gzFile : recordsDir.listFiles()) {
+            if ("gz".equals(FilenameUtils.getExtension(gzFile.getName()))){
+                    parseGedcomFile(gzFile, collectionLabels);
+                    System.out.println("\tParsed " + gzFile.getName());
+            }
+        }
+
+        File outfile = new File(outputDir, collectionName + ".csv");
+        writeLabels(collectionLabels, outfile);
+    }
+
+    private static void writeLabels(RecordAccumulator labels, File outfile)
+            throws IOException {
+        outfile.createNewFile();
+        PrintWriter writer = new PrintWriter(outfile);
+        labels.printAll(writer);
+        writer.close();
+        System.out.println("\tWrote records to " + outfile.getName());
+    }
+
+    /**
+     * Adds this gedcomx .gz file into this accumulator.
+     * */
+    private static void parseGedcomFile(File gzFile, RecordAccumulator output)
+            throws XMLStreamException, IOException {
         FileInputStream instream = new FileInputStream(gzFile);
         RecordSetIterator it = new RecordSetIterator(instream, true);
         while(it.hasNext()){
             Gedcomx g = it.next();
-            accumulator.addItem(g);
+            output.addItem(g);
         }
         it.close();
-        accumulator.printAll(output);
     }
 }
